61d97b64fd8f132e596586a27e530d72f5582d30
Vikoey04 <vikoey04@gmail.com>
2022-04-20 08:07:57 -0700

    Update entregable ex2

--- ex-entregables/ex2/ex2/MyGLWidget.cpp
+++ ex-entregables/ex2/ex2/MyGLWidget.cpp
@@ -49,3 +49,124 @@ MyGLWidget::~MyGLWidget()
 {
 
 }
+
+// Reescrbim paintGL de LL2
+void MyGLWidget::paintGL ()
+{
+  // descomentar per canviar paràmetres
+  // glViewport (0, 0, ample, alt);
+
+  // Esborrem el frame-buffer
+  glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  // Patricio
+  glBindVertexArray (VAO_Patr);
+  patrTransform();
+  glDrawArrays(GL_TRIANGLES, 0, patr.faces().size()*3);
+
+  // Pilota
+  glBindVertexArray (VAO_Pil);
+  pilTransform();
+  glDrawArrays(GL_TRIANGLES, 0, pil.faces().size()*3);
+
+  // Cub
+  glBindVertexArray (VAO_Cub);
+  paret1Transform();
+  glDrawArrays(GL_TRIANGLES, 0, 36);
+  paret2Transform();
+  glDrawArrays(GL_TRIANGLES, 0, 36);
+  paret3Transform();
+  glDrawArrays(GL_TRIANGLES, 0, 36);
+
+  // Terra
+  glBindVertexArray (VAO_Terra);
+  identTransform();
+  glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+  glBindVertexArray (0);
+}
+
+// Reescribim patrTransform de LL2
+void MyGLWidget::patrTransform() {
+  glm::mat4 TG(1.0f);
+  TG = glm::translate(TG, posPorter);
+  TG = glm::rotate(TG, -(float)M_PI/2, glm::vec3(0.0, 1.0, 0.0));
+  TG = glm::scale(TG, glm::vec3(escalaPatr/0.25, escalaPatr/0.25, escalaPatr/0.25));
+  TG = glm::translate(TG, -centreBasePatr);
+  glUniformMatrix4fv(transLoc, 1, GL_FALSE, &TG[0][0]);
+}
+
+void MyGLWidget::paret1Transform() {
+    glm::mat4 TG(1.0f);
+    TG = glm::translate(TG, glm::vec3(0, 0, -6.9));
+    TG = glm::scale(TG, glm::vec3(20, 2, 0.2));
+    glUniformMatrix4fv(transLoc, 1, GL_FALSE, &TG[0][0]);
+}
+
+void MyGLWidget::paret2Transform() {
+    glm::mat4 TG(1.0f);
+    TG = glm::translate(TG, glm::vec3(0, 0, 6.9));
+    TG = glm::scale(TG, glm::vec3(20, 2, 0.2));
+    glUniformMatrix4fv(transLoc, 1, GL_FALSE, &TG[0][0]);
+}
+
+void MyGLWidget::paret3Transform() {
+    glm::mat4 TG(1.0f);
+    TG = glm::translate(TG, glm::vec3(-9.9, 0, 0));
+    TG = glm::scale(TG, glm::vec3(0.2, 2, 14));
+    glUniformMatrix4fv(transLoc, 1, GL_FALSE, &TG[0][0]);
+}
+
+void MyGLWidget::viewTransform() {
+    glm::mat4 VM(1.0f);
+    VM = glm::translate(VM, glm::vec3(0, 0, -26.67));
+    VM = glm::rotate(VM, (float)M_PI/4, glm::vec3(1, 0, 0));
+    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &VM[0][0]);
+}
+
+// Reescribim keyPressEvent de LL2
+void MyGLWidget::keyPressEvent(QKeyEvent* event) 
+{
+  makeCurrent();
+  switch (event->key()) {
+    case Qt::Key_Up: { // moviment de la pilota
+      if (posPilota[0] == 7.0)
+        mourePilota ();
+      break;
+    }
+    case Qt::Key_I: { // reinicia posició pilota
+      iniciPilota ();
+      break;
+    }
+
+    case Qt::Key_Left: {
+        if (posPorter[2] != 5) {
+            posPorter[2] += 0.5;
+            mouPorterLeft();
+        }
+        break;
+    }
+    case Qt::Key_Right: {
+        if (posPorter[2] != -5) {
+            posPorter[2] -= 0.5;
+            mouPorterRight();
+        }
+        break;
+    }
+
+    default: event->ignore(); break;
+  }
+  update();
+}
+
+void MyGLWidget::mouPorterLeft() {
+    glm::mat4 TG(1.0f);
+    TG = glm::translate(TG, glm::vec3(0, 0, 0.5));
+    glUniformMatrix4fv(transLoc, 1, GL_FALSE, &TG[0][0]);
+}
+
+void MyGLWidget::mouPorterRight() {
+    glm::mat4 TG(1.0f);
+    TG = glm::translate(TG, glm::vec3(0, 0, -0.5));
+    glUniformMatrix4fv(transLoc, 1, GL_FALSE, &TG[0][0]);
+}
--- ex-entregables/ex2/ex2/MyGLWidget.h
+++ ex-entregables/ex2/ex2/MyGLWidget.h
@@ -9,6 +9,22 @@ class MyGLWidget : public LL2GLWidget {
     MyGLWidget(QWidget *parent=0) : LL2GLWidget(parent) {}
     ~MyGLWidget();
 
+  protected:
+    virtual void paintGL(); // De LL2, la reescribim
+
+    virtual void patrTransform(); //De LL2, la reescribim
+
+    virtual void viewTransform(); // De LL2, la reescribim
+
+    virtual void keyPressEvent(QKeyEvent* event); // De LL2, la reescribim
+
   private:
     int printOglError(const char file[], int line, const char func[]);
+
+    void paret1Transform();
+    void paret2Transform();
+    void paret3Transform();
+
+    void mouPorterLeft();
+    void mouPorterRight();
 };
